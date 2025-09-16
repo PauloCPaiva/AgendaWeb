40c441fbb9e0cb853960b03a582e9074b5c24fa5
PauloC.Jr <paulocesarapjr@gmail.com>
2025-09-16 14:46:16 -0300

    commit aula 14

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,32 @@ import { Routes } from '@angular/router';
 import { CadastroTarefas } from './components/pages/cadastro-tarefas/cadastro-tarefas';
 import { ConsultaTarefas } from './components/pages/consulta-tarefas/consulta-tarefas';
 import { EdicaoTarefas } from './components/pages/edicao-tarefas/edicao-tarefas';
+import { Dashboard } from './components/pages/dashboard/dashboard';
+
 
 export const routes: Routes = [
     {
+        path : 'pages/dashboard',
+        component: Dashboard
+    },
+    {
         path : 'pages/cadastro-tarefas',
         component: CadastroTarefas
-    } ,
+    },
     {
         path : 'pages/consulta-tarefas',
         component: ConsultaTarefas
-    } ,
+    },
     {
         path : 'pages/edicao-tarefas',
-        component:  EdicaoTarefas
-    } ,
+        component: EdicaoTarefas
+    },
     {
-        path:'', pathMatch: 'full',
-        redirectTo: '/pages/consulta-tarefas'
+        path: '', pathMatch: 'full',
+        redirectTo: '/pages/dashboard'
     }
 ];
+
+
+
+
